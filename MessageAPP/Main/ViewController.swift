4f95da03d6ea4aae39aa5583fb60b2a8a7a1ac29
Mauro Figueiredo <mauro.figueiredo@coopertec.com.br>
2021-03-22 16:55:18 -0300

    commit teste

--- MessageAPP/Main/ViewController.swift
+++ MessageAPP/Main/ViewController.swift
@@ -13,19 +13,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var backView: UIView!
     @IBOutlet weak var detailView: UIView!
     @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var signInLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.navigationBar.isHidden = true
-        
-        
         setupUI()
     }
     
     func setupUI(){
         topView.roundCorners(.bottomLeft, radius: 50)
         detailView.roundCorners(.topRight, radius: 40)
-        
+      
         guard let otherColor = GradientColors(rawValue: "dirtyFog") else {return}
         let gradientLayer: CAGradientLayer = CAGradientLayer()
         
@@ -37,8 +37,23 @@ class ViewController: UIViewController {
         
         topView.layer.insertSublayer(gradientLayer, below: topView.layer.sublayers?.last)
         backView.backgroundColor = otherColor.gradient.second
+
+        setGradientToLabel(gradientLayer: gradientLayer)
     }
     
+    func setGradientToLabel(gradientLayer: CAGradientLayer){
+         let teste = gradientLayer
+        //teste.frame = CGRect(x: 0.0, y: 0.0, width: signInLabel.frame.size.width, height: signInLabel.frame.size.height)
+         signInLabel.textColor = gradientColor(bounds: signInLabel.bounds, gradientLayer: teste)
+    }
+    
+    func gradientColor(bounds: CGRect, gradientLayer : CAGradientLayer) -> UIColor? {
+        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
+        gradientLayer.render(in: UIGraphicsGetCurrentContext()!)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return UIColor(patternImage: image!)
+    }
     @IBAction func loginButton(_ sender: Any) {
         if let vc = UIStoryboard(name: "Chats", bundle: nil).instantiateInitialViewController() as? ChatsViewController {
             navigationController?.pushViewController(vc, animated: true)
